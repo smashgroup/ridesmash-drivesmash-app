c93da720b26d154eb80668f7726ee958a0262229
G-HORIZON <ridesmashnigeria.com>
2024-12-16 15:01:44 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const {WebSocketServer} = require("ws");
+const geolib = require("geolib");
+
+const app = express();
+const PORT = 4000;
+
+// store drivers locations
+
+let drivers = {};
+
+// create websocket server
+
+const wss = new WebSocketServer({ port: 8080});
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        try {
+            const data = JSON.parse(message);
+            console.log(`Received message: `, data);
+
+            if (data.type === "locationUpdate" && data.role === "driver") {
+                drivers[data.driver] = {
+                    latitude: data.data.latitude,
+                    longitude: data.data.longitude,
+                }
+                console.log(`updated driver location:`, drivers[data.driver])
+            }
+
+            if (data.type === "requestRide" && data.role === "user") {
+                const nearbyDrivers = findNearbyDrivers(data.latitude, data.longitude);
+
+                ws.send(
+                    JSON.stringify({type: "nearbyDrivers", drivers: nearbyDrivers})
+                )
+            }
+
+        } catch (error) {
+            console.log('Failed to parse Websocket message', error)
+        }
+    })
+})
+
+const findNearbyDrivers = (userLat, userLon) => {
+    return Object.entries(drivers).filter(([id,location]) => {
+        const distance = geolib.getDistance({
+            latitude: userLat, 
+            longitude: userLon
+        }, location);
+        return distance <= 5000  // 5kilometers
+    })
+    .map(([id, location]) => ({id, ...location}));
+};
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
